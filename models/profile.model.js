e31ffd24ec7338311a37b1447b087bd410d4b9ef
onomdev <ikonomirei0@gmail.com>
2021-12-29 01:04:53 +0100

    First deploy to Heroku

---
+++ models/profile.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const Profile = Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: String,
+    DOB: String,
+    about: String,
+    img: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+module.exports = mongoose.model("Profile", Profile);
